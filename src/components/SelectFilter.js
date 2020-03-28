db8bb5e5875023d7c5e63512a018f1079d13f6f1
sreejith97 <47489986+sreejith97@users.noreply.github.com>
2020-03-28 13:09:04 +0530

    Add files via upload

--- src/components/SelectFilter.js
+++ src/components/SelectFilter.js
@@ -22,7 +22,7 @@ function SelectFilter({setFilter ,posts}) {
       </p>
       </div>
       <div className="ratingFilter">
-      <p>Select Rting <br></br>
+      <p>Select Rating <br></br>
       <button className="btn btn-secondary btn-sm m-2" onClick={() => setRFilter(1)}>Above 1</button>
       <button className="btn btn-secondary btn-sm m-2" onClick={() => setRFilter(2)}>Above 2</button>
       <button className="btn btn-secondary btn-sm m-2" onClick={() => setRFilter(3)}>Above 3</button>
@@ -32,7 +32,6 @@ function SelectFilter({setFilter ,posts}) {
       
 
 
-      {/* <button  onClick={() =>  handleFilterone({rFilter})}>test</button> */}
       <button className="btn btn-outline-primary" onClick={() => setFilter(posts.filter(person => person.rating >= rFilter && person.type === tFilter  ))}>Apply </button><br/><br/>
       <button className="btn btn-outline-warning" onClick={() => setFilter(posts)}>Remove Filters</button>
         </div>
